b80d3a9f0c280e43a9b4c6889ed76182d2b2a69a
John  Tucker <john@larkintuckerllc.com>
2017-12-05 08:32:37 -0500

    Production

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-const BASE = `${window.location.protocol}//${window.location.hostname}`;
+const BASE = process.env.NODE_ENV === 'production' ?
+  'http://192.168.1.2' :
+  'http://localhost';
 export const BASE_URL_HTTP = `${BASE}:3000`;
 export const BASE_URL_SOCKET = `${BASE}:3001`;
 export const CHANNELS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
