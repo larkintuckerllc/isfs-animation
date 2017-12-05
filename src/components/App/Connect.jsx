5b8c647c7e1f1033e8eda18e4706906e1128a9f5
John  Tucker <john@larkintuckerllc.com>
2017-12-04 22:16:46 -0500

    final

--- src/components/App/Connect.jsx
+++ src/components/App/Connect.jsx
@@ -1,23 +1,44 @@
 import { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
+import { MATRIX, DIMENSIONS } from '../../config';
+import { grid } from '../../util/grid';
 import * as fromConnected from '../../ducks/connected';
 import * as fromChannel from '../../ducks/channel';
 
 class Connect extends Component {
   componentDidMount() {
-    const { disconnect, thr0wConnect, setChannel } = this.props;
+    const {
+      channel,
+      disconnect,
+      thr0wConnect,
+      setChannel,
+    } = this.props;
     let connecting = true;
     function repeat() {
       if (!connecting) return;
       thr0wConnect()
         .then(
-          () => { connecting = false; },
+          () => {
+            connecting = false;
+            const frameEl = document.getElementById('frame');
+            const frameContentEl = document.getElementById('frame__content');
+            grid(channel, frameEl, frameContentEl, MATRIX, DIMENSIONS);
+          },
           () => window.setTimeout(repeat, 5000),
         );
     }
     function checkEsc(e) {
       if (e.keyCode !== 27) return;
+      const frameEl = document.getElementById('frame');
+      const frameContentEl = document.getElementById('frame__content');
+      frameContentEl.style.width = '100%';
+      frameContentEl.style.height = '100%';
+      frameContentEl.style.left = '0px';
+      frameContentEl.style.top = '0px';
+      frameEl.style.width = '100%';
+      frameEl.style.height = '100%';
+      frameEl.style.transform = '';
       document.removeEventListener('keydown', checkEsc);
       connecting = false;
       setChannel(null);
@@ -33,12 +54,15 @@ class Connect extends Component {
   }
 }
 Connect.propTypes = {
+  channel: PropTypes.number.isRequired,
   disconnect: PropTypes.func.isRequired,
   setChannel: PropTypes.func.isRequired,
   thr0wConnect: PropTypes.func.isRequired,
 };
 export default connect(
-  null,
+  state => ({
+    channel: fromChannel.getChannel(state),
+  }),
   {
     disconnect: fromConnected.disconnect,
     setChannel: fromChannel.setChannel,
--- src/config.js
+++ src/config.js
@@ -1,3 +1,30 @@
 const BASE = `${window.location.protocol}//${window.location.hostname}`;
 export const BASE_URL_HTTP = `${BASE}:3000`;
 export const BASE_URL_SOCKET = `${BASE}:3001`;
+export const MATRIX = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8, 9],
+];
+export const DIMENSIONS = [{
+  width: 1920,
+  height: 1080,
+  scale: 0.84,
+  padding: 0,
+  spacing: 28,
+  margin: 20,
+}, {
+  width: 1920,
+  height: 1080,
+  scale: 0.84,
+  padding: 0,
+  spacing: 28,
+  margin: 60,
+}, {
+  width: 1080,
+  height: 1920,
+  scale: 1,
+  padding: 111,
+  spacing: 112,
+  margin: 0,
+}];
