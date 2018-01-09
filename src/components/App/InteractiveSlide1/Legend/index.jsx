65cdcd3b2e7b6ea344c96c820914fd81f2a7a982
John  Tucker <john@larkintuckerllc.com>
2018-01-09 09:53:49 -0500

    Fix font

--- src/components/App/InteractiveSlide1/Legend/index.jsx
+++ src/components/App/InteractiveSlide1/Legend/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import getQueryString from '../../../../util/getQueryString';
 import styles from './index.scss';
 
+const wall = getQueryString('wall');
 function Legend({ items }) {
   return (
     <div id={styles.root}>
       {items.map(item => (
-        <div className={styles.rootItem}>{item}</div>
+        <div className={wall === null ? styles.rootItem : `${styles.rootItem} ${styles.rootItemWall}`}>{item}</div>
       ))}
     </div>
   );
